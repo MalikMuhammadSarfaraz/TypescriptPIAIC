c5f1922fc8b76db59f1e1ada0fe2e59d3e838905
Malik Muhammad Sarfaraz <107793257+MalikMuhammadSarfaraz@users.noreply.github.com>
2023-10-02 14:34:35 +0500

    JSON Object & Array

---
+++ jsonObj.js
@@ -0,0 +1,15 @@
+"use strict";
+// JSON TEXT ( typescript not allow to write text in JSON format)
+//     // {
+//     // "name": "Sarfaraz",
+//     // "age": 31,
+//     // "gender": "male",
+// }
+// JSON object
+const myobject = { "name": "Sarfaraz", "age": 31 };
+console.log(myobject.name);
+// Sarfaraz
+//Array
+const myarray = ["Ali", "Irfan", "Dilshaad"];
+console.log(myarray[2]);
+// Dilshaad
---
+++ jsonObj.ts
@@ -0,0 +1,25 @@
+
+// JSON TEXT ( typescript not allow to write text in JSON format)
+
+//     // {
+//     // "name": "Sarfaraz",
+//     // "age": 31,
+//     // "gender": "male",
+// }
+
+ // JSON object
+
+    const myobject =  {"name" : "Sarfaraz" , "age" : 31}
+        console.log(myobject.name);
+    
+    // Sarfaraz
+
+    //Array
+    const myarray:String[] = ["Ali", "Irfan", "Dilshaad"]
+    console.log(myarray[2]); 
+    // Dilshaad
+
+
+
+
+
